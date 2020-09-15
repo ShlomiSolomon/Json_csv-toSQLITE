fa7d058e7b1b1ff8f811e1b3797b27ecbec933e3
ShlomiSolomon <47434476+ShlomiSolomon@users.noreply.github.com>
2020-09-15 14:20:08 +0300

    Update reciever.py

--- reciever.py
+++ reciever.py
@@ -6,8 +6,8 @@ from sender import stringJsonMsg
 import pandas as pd
 
 
-# define a new function to init the key-value of the json msg and call to :db build
 
+# define a new function to init the key-value of the json msg and call to :db build
 def parse_recievedMsg(data):
     json_data = json.loads(data)
     file_path = json_data['file-path']
@@ -35,13 +35,6 @@ def db_build(file_path, format, table_name):
     except Error as e:
         print(e)
 
-# build the message as string in json structure using the array and the path const  ,'table name':table_name
-stringJsongraph = {'table-name': table_name}
-# convert the string into json valid format
-convertStringToJsonData = json.dumps(stringJsongraph)
-print(convertStringToJsonData)
-
-                                                        # # # MQ # # #
 
 connection = pika.BlockingConnection( pika.ConnectionParameters(host= 'localhost' ) )
 channel = connection.channel()
@@ -52,6 +45,13 @@ def callback(ch,method,properties,body):
     data = body
     parse_recievedMsg(data)
     print(("[x] received %s" % data))
+    json_data = json.loads(data)
+    table_name = json_data['table-name']
+    stringJsongraph = {'table-name': table_name}
+    # convert the string into json valid format
+    convertStringToJsonData = json.dumps(stringJsongraph)
+    channel.basic_publish(exchange='', routing_key='graph', body=convertStringToJsonData)
+    print("[x] sent end of load and run %s graph " %convertStringToJsonData )
 
 
 channel.basic_consume(queue='message', auto_ack=True, on_message_callback=callback)
@@ -60,20 +60,3 @@ try:
     channel.start_consuming()
 finally:
     connection.close()
-
-
-#
-# channel = connection.channel()
-# #declare on the queue name we are going to listen to on the recieve.py class
-# channel.queue_declare(queue='graph')
-# #public the channel with the given info/params
-# #exchange    == (str or unicode) – The exchange to publish to
-# #routing_key == (str or unicode) – The routing key to bind on
-# #body        == (str or unicode) – The message body
-# channel.basic_publish(exchange='', routing_key='graph', body=convertStringToJsonData)
-# print("[x] sent end of load and run %s graph " %convertStringToJsonData )
-# #close the connection when finish
-# try:
-#     channel.start_consuming()
-# finally:
-#     connection.close()
